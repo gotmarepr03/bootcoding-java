70b62d2d940f2e9eaedea7684764e7310235e656
PRANAY GOTMARE <gotmarepr0@gmail.com>
2023-01-31 21:12:23 +0530

    Day 3

---
+++ Recurssion/NaturalRecursion.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class NaturalRecursion {
+
+    static int count = 0;
+
+    public static void Recurssion(int n) {
+
+        if (count < n) {
+
+            count++;
+
+            System.out.print(count + " ");
+
+            Recurssion(n);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter the number:-");
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println("Natural numbers upto 50 :--");
+
+        Recurssion(n);
+    }
+
+}
